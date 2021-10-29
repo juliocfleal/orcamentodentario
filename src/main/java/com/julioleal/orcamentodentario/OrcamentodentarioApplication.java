12eefb28ba2c3fe339f5e3218f96054ce8ddd844
Julio Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2021-10-28 23:29:52 -0300

    project created

---
+++ src/main/java/com/julioleal/orcamentodentario/OrcamentodentarioApplication.java
@@ -0,0 +1,13 @@
+package com.julioleal.orcamentodentario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentodentarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentodentarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julioleal/orcamentodentario/OrcamentodentarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioleal.orcamentodentario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentodentarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
